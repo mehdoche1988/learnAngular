c5e06a3e6b77d10bbcc26a489fd875cd749c6da7
mehdoche1988 <mehdi.semlali.1988@gmail.com>
2020-10-29 12:31:56 +0100

    add img +name

--- src/app/mock-horror.ts
+++ src/app/mock-horror.ts
@@ -5,70 +5,70 @@ export const HORRORS: Horror[] = [
         id: 1,
         name: "Freddy Krueger",
         movie: "Les Griffes de la nuit",
-        picture: "https://upload.wikimedia.org/wikipedia/en/thumb/e/eb/Freddy_Krueger_%28Robert_Englund%29.jpg/220px-Freddy_Krueger_%28Robert_Englund%29.jpg",
+        picture: "https://fr.web.img4.acsta.net/c_215_290/pictures/14/11/05/11/44/080245.jpg",
         created: new Date()
     },
     {
         id: 2,
-        name: "Jason Vorhees",
+        name: "Jason Voorhees",
         movie: "Vendredi 13 ",
-        picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/004.png",
+        picture: "https://fr.web.img3.acsta.net/c_215_290/medias/nmedia/18/66/93/20/19050019.jpg",
         created: new Date()
     },
     {
         id: 3,
-        name: "Grippe-Sou",
+        name: "Pennywise",
         movie: "Ça",
-        picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/007.png",
+        picture: "https://fr.web.img2.acsta.net/c_215_290/pictures/17/03/29/14/40/513263.jpg",
         created: new Date()
     },
     {
         id: 4,
         name: "Leatherface",
         movie: "Massacre à la tronçonneuse",
-        picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/007.png",
+        picture: "https://fr.web.img4.acsta.net/c_215_290/pictures/14/09/25/13/08/561794.jpg",
         created: new Date()
     },
     {
         id: 5,
         name: " Michael Myers",
         movie: "Halloween",
-        picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/007.png",
+        picture: "https://fr.web.img6.acsta.net/c_215_290/pictures/18/09/19/14/32/4251359.jpg",
         created: new Date()
     },
     {
         id: 6,
         name: "Norman Bates",
         movie: "Psycho",
-        picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/007.png",
+        picture: "https://fr.web.img3.acsta.net/c_215_290/medias/nmedia/18/35/44/77/18672162.jpg",
         created: new Date()
     },
     {
         id: 7,
         name: "Ghostface",
         movie: "The Scream",
-        picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/007.png",
+        picture: "https://fr.web.img4.acsta.net/c_215_290/medias/nmedia/18/36/12/88/18454857.jpg",
         created: new Date()
     },
     {
         id: 8,
         name: "Pinhead",
         movie: "Hellraiser",
-        picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/007.png",
+        picture: "https://fr.web.img6.acsta.net/c_215_290/medias/nmedia/18/63/14/44/18946269.jpg",
         created: new Date()
     },
     {
         id: 9,
         name: "Chucky",
         movie: "Jeu d’enfant",
-        picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/007.png",
+        picture: "https://fr.web.img6.acsta.net/c_215_290/medias/nmedia/18/70/93/92/20482627.jpg",
         created: new Date()
     },
     {
         id: 10,
         name: "The Jigsaw Killer",
         movie: "Saw ",
-        picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/007.png",
+        picture: "https://fr.web.img4.acsta.net/c_215_290/pictures/14/10/18/13/11/145703.jpg",
         created: new Date()
     }
 
